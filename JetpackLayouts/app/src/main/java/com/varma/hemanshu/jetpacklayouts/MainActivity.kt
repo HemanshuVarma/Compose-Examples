188a92c9fedf3d06924997ed8d73a13ca0a7e1b0
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-09-20 19:59:40 +0530

    Add: Simple view with click listener and modifiers

--- JetpackLayouts/app/src/main/java/com/varma/hemanshu/jetpacklayouts/MainActivity.kt
+++ JetpackLayouts/app/src/main/java/com/varma/hemanshu/jetpacklayouts/MainActivity.kt
@@ -3,10 +3,25 @@ package com.varma.hemanshu.jetpacklayouts
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.EmphasisAmbient
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideEmphasis
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.varma.hemanshu.jetpacklayouts.ui.JetpackLayoutsTheme
 
@@ -17,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             JetpackLayoutsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerCard()
                 }
             }
         }
@@ -25,14 +40,41 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = {
+                //Handle click here
+            })
+            .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.preferredSize(56.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .gravity(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            ProvideEmphasis(emphasis = EmphasisAmbient.current.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackLayoutsTheme {
-        Greeting("Android")
+        PhotographerCard()
     }
 }
