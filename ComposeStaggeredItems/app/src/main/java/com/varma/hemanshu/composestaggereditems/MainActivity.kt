9bce3d89c69111cda6437b4dc88d1ae3366f39c2
Hemanshu Varma <hemanshuvarma916@gmail.com>
2020-09-27 23:53:25 +0530

    Add: Staggered Core Logic in StaggeredItems

--- ComposeStaggeredItems/app/src/main/java/com/varma/hemanshu/composestaggereditems/MainActivity.kt
+++ ComposeStaggeredItems/app/src/main/java/com/varma/hemanshu/composestaggereditems/MainActivity.kt
@@ -2,10 +2,11 @@ package com.varma.hemanshu.composestaggereditems
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Layout
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
 import com.varma.hemanshu.composestaggereditems.ui.ComposeStaggeredItemsTheme
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
             ComposeStaggeredItemsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -25,14 +26,71 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3, //Number of rows for staggered items, default set to 3
+    children: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        children = children
+    ) { measurable, constraints ->
+
+        // Keep track of the width of each row
+        val rowWidths = IntArray(rows) { 0 }
+
+        // Keep track of the max height of each row
+        val rowMaxHeights = IntArray(rows) { 0 }
+
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurable.mapIndexed { index, measurable ->
+
+            // Measure each child
+            val placeable = measurable.measure(constraints)
+
+            // Track the width and max height of each row
+            val row = index % rows
+            rowWidths[row] = rowWidths[row] + placeable.width
+            rowMaxHeights[row] = kotlin.math.max(rowMaxHeights[row], placeable.height)
+
+            placeable
+        }
+
+        // Grid's width is the widest row
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        // Grid's height is the sum of the tallest element of each row
+        // coerced to the height constraints
+        val height = rowMaxHeights.sumBy { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        // Y of each row, based on the height accumulation of previous rows
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowMaxHeights[i - 1]
+        }
+        // Set the size of the parent layout
+        layout(width, height) {
+            // x cord we have placed up to, per row
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStaggeredItemsTheme {
-        Greeting("Android")
     }
 }
